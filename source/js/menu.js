8102ecc7c9f1c329ff8c4a18980e80b9ccdd162c
vrozumkov <vrozumkov@ukr.net>
2022-02-07 18:03:23 +0200

    правки

--- source/js/menu.js
+++ source/js/menu.js
@@ -2,6 +2,7 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+document.querySelector('.page-header--nojs').classList.remove('page-header--nojs');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
